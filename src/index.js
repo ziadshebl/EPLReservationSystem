029f18cf4ce0f1a7b35d145455665676cf542068
Abdelrahman-Shahda <abdelrahmanshahda@yahoo.com>
2020-12-27 06:52:13 +0200

    Adding MongoDBURL and .env File

---
+++ src/index.js
@@ -0,0 +1,36 @@
+require('./db/mongoose')
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+//Different routes
+// const registerRouter = require('./routers/register.js')
+// const userRouter = require('./routers/user.js')
+// const postRouter = require('./routers/post.js')
+// const adminRouter = require('./routers/admin')
+// const supplierRouter = require('./routers/supplier')
+// const storeRouter = require('./routers/store.js')
+//Configuring the app
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cors({
+    credentials:true,
+    origin: '*'
+}))
+
+// app.use('/register',registerRouter)
+// app.use('/user', userRouter)
+// app.use('/posts', postRouter)
+// app.use('/admin', adminRouter)
+// app.use('/supplier', supplierRouter)
+// app.use('/store', storeRouter)
+app.get('/', (req,res) => {
+
+    res.send("Gamed");
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('Server is Up on', process.env.PORT)
+})
